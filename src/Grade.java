778fa1409cd9ed479c965864dc266d855990f6a7
joao <joao182cx@gmail.com>
2019-01-29 10:36:08 -0200

    Utilização de Rectangle2D.Double.

--- src/Grade.java
+++ src/Grade.java
@@ -1,4 +1,6 @@
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
 
 import javax.swing.JPanel;
 
@@ -8,16 +10,19 @@ public class Grade extends JPanel{
 	@Override
 	public void paintComponent(Graphics g) {
 		
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D) g;
+		
 		for(int x = getWidth()/10; x < getWidth()/10*9; x += getWidth()/10) {
 			
-			g.drawRect(x, -1, getWidth()/10, getHeight() + 1);
-			
+			//g.drawRect(x, -1, getWidth()/10, getHeight() + 1);
+			g2d.draw(new Rectangle2D.Double(x, -1, getWidth()/10, getHeight() + 1));
 		}
 		
 		for(int y = getHeight()/10; y < getHeight()/10*9; y += getHeight()/10) {
 			
-			g.drawRect(-1, y, getWidth() + 1, getHeight()/10);
-			
+			//g.drawRect(-1, y, getWidth() + 1, getHeight()/10);
+			g2d.draw(new Rectangle2D.Double(-1, y, getWidth() + 1, getHeight()/10));
 		}
 		
 	}
